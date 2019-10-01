7b2f433b2575450f2df1e6dd3a6eecfa9440e9c4
lHasib <hasibuzzaman25@gmail.com>
2019-10-01 19:13:02 +0600

    ProjectAdded

---
+++ MyConsoleApp/MyConsoleApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "";
+            string age = "";
+            string address = "";
+            string contact = "";
+            Console.WriteLine("Please Enter your Name: ");
+            name = Console.ReadLine();
+            Console.WriteLine("Please Enter your Age: ");
+            age = Console.ReadLine();
+            Console.WriteLine("Please Enter your Address: ");
+            address = Console.ReadLine();
+            Console.WriteLine("Please Enter your Contact NO: ");
+            contact = Console.ReadLine();
+            Console.WriteLine("Name: " + name + "\n" + "Age: " + age + "\n" + "Address: " + address + "\n" + "Contact No: " + contact + "\n");
+            Console.ReadKey();
+
+
+
+
+
+
+        }
+    }
+}
